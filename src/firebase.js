564cf7aded66aac7dc5c343a76010f22415a981f
imadeda <152673708+imadeda@users.noreply.github.com>
2025-11-26 23:57:52 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCR5PCIaJWvi7hKmRTJwE9HBGz8MTF-S-w",
   authDomain: "up-system-d12fc.firebaseapp.com",
@@ -15,6 +11,5 @@ const firebaseConfig = {
   appId: "1:100913259554:web:bc733ae3ff5bf35233567b"
 };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
